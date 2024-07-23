7bd0557990cea6c618c4ffd4732be6d89417ceef
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-07-23 23:24:37 +0100

    ci: knip

--- src/types/github.ts
+++ src/types/github.ts
@@ -4,8 +4,6 @@ import { Context } from "./context";
 export type Issue = RestEndpointMethodTypes["issues"]["get"]["response"]["data"];
 export type IssueComments = RestEndpointMethodTypes["issues"]["listComments"]["response"]["data"];
 export type ReviewComments = RestEndpointMethodTypes["pulls"]["listReviewComments"]["response"]["data"];
-export type IssueComment = RestEndpointMethodTypes["issues"]["listComments"]["response"]["data"][0];
-export type ReviewComment = RestEndpointMethodTypes["pulls"]["listReviewComments"]["response"]["data"][0];
 
 export type FetchParams = {
   context: Context;
